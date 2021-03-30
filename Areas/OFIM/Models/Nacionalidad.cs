ed4d1c5593967a05a7a9ade102226826eb161db0
LUIS CARRILLO VILLARREAL <luis.carrillo.villarreal@est.una.ac.cr>
2021-03-29 19:20:41 -0600

    algunas clases con informacion

--- Areas/OFIM/Models/Nacionalidad.cs
+++ Areas/OFIM/Models/Nacionalidad.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SYGESTMunicipalSync.Areas.OFIM.Models
 {
-    public class Nacionalidad
+    public partial class Nacionalidad
     {
+        [Display(Name = "Id:")]
+        public int NacionalidadId { get; set; }
+
+        [Required(ErrorMessage = "Debe digitar el Nombre de la nacionalidad")]
+        [Display(Name = "Nombre:")]
+        public string Nombre { get; set; }
+
+        public virtual ICollection<PersonaOFIM> PersonaOFIM { get; set; }
+        public Nacionalidad()
+        {
+            PersonaOFIM = new HashSet<PersonaOFIM>();
+        }
+
     }
 }
--- Areas/OFIM/Models/Padecimientos.cs
+++ Areas/OFIM/Models/Padecimientos.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SYGESTMunicipalSync.Areas.OFIM.Models
 {
-    public class Padecimientos
+    public partial class Padecimientos
     {
+        [Display(Name = "Id:")]
+        public int PadecimientoId { get; set; }
+
+        [Required(ErrorMessage = "Debe digitar el nombre del padecimiento")]
+        [Display(Name = "Nombre:")]
+        public string Nombre { get; set; }
+
+        [Display(Name = "Descripcion:")]
+        [Required(ErrorMessage = "Debe digitar la descripción del padecimiento")]
+        [StringLength(200, ErrorMessage = "Ha excedido los 200 caracteres")]
+        public string Descripcion { get; set; }
+
+        public virtual ICollection<PersonaOFIM> PersonaOFIM { get; set; }
+        public Padecimientos()
+        {
+            PersonaOFIM = new HashSet<PersonaOFIM>();
+        }
     }
 }
