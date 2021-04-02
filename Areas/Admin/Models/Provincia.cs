4be5a5411e2ce5fe6af8f9b83114ea129eaa1ce7
LUIS CARRILLO VILLARREAL <luis.carrillo.villarreal@est.una.ac.cr>
2021-04-01 21:42:58 -0600

    modificacion de modelo

--- Areas/Admin/Models/Provincia.cs
+++ Areas/Admin/Models/Provincia.cs
@@ -25,11 +25,11 @@ namespace SYGESTMunicipalSync.Areas.Admin.Models
         public int DistritoId { get; set; }
         public virtual Distrito Distrito { get; set; }
         public virtual ICollection<Persona> Persona { get; set; }
-        public virtual ICollection<Denuncia> Denuncia { get; set; }
+        public virtual ICollection<Usuario> Usuario { get; set; }
         public Provincia()
         {
             Persona = new HashSet<Persona>();
-            Denuncia = new HashSet<Denuncia>();
+            //Usuario = new HashSet<Usuario>();
         }
     }
 }
---
+++ Areas/Admin/Models/ViewModel/RolUsuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SYGESTMunicipalSync.Areas.Admin.Models.ViewModel
+{
+    public partial class RolUsuario
+    {
+
+        [Display(Name = "Rol Usuario Id:")]
+        public int TipoUsuarioId { get; set; }
+
+        [Display(Name = "Tipo de Usuario Id:")]
+        public int RolId { get; set; }
+
+        [Display(Name = "Tipo de Usuario:")]
+        public virtual TipoUsuario TipoUsuario { get; set; }
+
+        [Display(Name = "Usuario Id:")]
+        public int UsuarioId { get; set; }
+
+        [Display(Name = "Tipo de Usuario:")]
+        public virtual Usuario Usuario { get; set; }
+    }
+}
