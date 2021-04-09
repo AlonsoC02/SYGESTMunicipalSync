e39b5df692d17f4908be4def64629f55d1e230de
LUIS CARRILLO VILLARREAL <luis.carrillo.villarreal@est.una.ac.cr>
2021-04-08 20:40:28 -0600

    puntos de recuperacion controller

---
+++ Areas/OFGA/Controllers/PuntoRecMaterialController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SYGESTMunicipalSync.Areas.OFGA.Models.ViewModels;
+using SYGESTMunicipalSync.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SYGESTMunicipalSync.Areas.OFGA.Controllers
+{
+    [Area("OFGA")]
+    public class PuntoRecMaterialController : Controller
+    {
+        private readonly DBSygestContext _db;
+        List<PuntoRecMaterialMaterialesClasificacionViewModel> listaPunto = new List<PuntoRecMaterialMaterialesClasificacionViewModel>();
+        static List<PuntoRecMaterialMaterialesClasificacionViewModel> lista = new List<PuntoRecMaterialMaterialesClasificacionViewModel>();
+        static private string _Fecha;
+        public PuntoRecMaterialController(DBSygestContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            listaPunto = (from medico in _db.PuntoRecMaterial
+                           join especialidad in _db.Clasificacion
+                           on medico.ClasificacionId equals especialidad.ClasificacionId
+
+                          join distrito in _db.Distrito
+                          on medico.DistritoId equals distrito.DistritoId
+
+                          join material in _db.Materiales
+                          on medico.MaterialId equals material.MaterialId
+
+                          select new PuntoRecMaterialMaterialesClasificacionViewModel
+                          {
+                              PuntosRecMaterialId = medico.PuntosRecMaterialId,
+                               Peso = medico.Peso,
+                               Distrito=distrito.Nombre,
+                               Clasificacion = especialidad.Nombre,
+                               Material= material.Nombre,
+                               Fecha = medico.Fecha
+                           }).ToList();
+            lista = listaPunto;
+            return View(listaPunto);
+        }
+    }
+}
